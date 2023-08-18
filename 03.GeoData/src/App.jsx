438c2c3bb9b61f3ef492e3b5a16702c781347d5c
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-18 18:03:19 +0200

    add tp IDE

--- 03.GeoData/src/App.jsx
+++ 03.GeoData/src/App.jsx
@@ -1,32 +1,27 @@
 import { useEffect, useState } from "react";
 import ListCard from "./components/ListCard";
 
-
 function App() {
-const [countries, setCountries] = useState(null)
+  const [countries, setCountries] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/region/europe")
+      .then((data) => data.json()) 
+      .then((data) => {
+        data.sort((a, b) => { 
+          console.log(data);
+          if (a.name.common < b.name.common) { 
+            return -1;
+          } else if (a.name.common > b.name.common) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
 
-  fetch('https://restcountries.com/v3.1/region/europe')
-  .then(data => data.json())
-  .then(data => {
-
-    data.sort((a,b) => {
-console.log(data);
-      if(a.name.common < b.name.common) {
-        return -1
-      } else if (a.name.common > b.name.common) {
-        return 1
-      } else {
-        return 0
-      }
-    })
-
-    setCountries(data)
-
-  })
-
-}, [])
+        setCountries(data);
+      });
+  }, []);
 
   return (
     <div className="min-h-screen bg-slate-800">
@@ -37,16 +32,12 @@ console.log(data);
         Click on a card to reveal a country's information
       </p>
       {countries && (
-  <ul className="grid min-[450px] grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
-    {countries.map((country, index) => (
-      <ListCard key={index} country={country} />
-    ))}
-  </ul>
-)}
-
-
-
-
+        <ul className="grid min-[450px] grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
+          {countries.map((country, index) => (
+            <ListCard key={index} country={country} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
